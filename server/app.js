9bffbeaad0d074d02bf89915f8f6e5e37d578d20
pgol <pgol200014@gmail.com>
2015-08-23 20:39:34 +0200

    add tests

--- server/app.js
+++ server/app.js
@@ -1,35 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Expense = require('../models/expenseModel');
 
 
 var db = mongoose.connect('mongodb://localhost/expenses');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var router = express.Router();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+var expenseRouter = require('../routes/expenseRoutes')(Expense);
+app.use('/api/expenses', expenseRouter);
 
-var Expense = require('../models/expenseModel');
-
-router.route('/expenses')
-    .get(function (req, res) {
-        var query = {};
-
-        if(req.query.category) {
-            query.category = req.query.category;
-        }
-
-        Expense.find(query, function (err, books) {
-
-            if (err) {
-                console.log(err);
-            } else {
-                res.json(books);
-            }
-        });
-    });
-
-app.use('/api', router);
 
 
 app.listen(port, function () {
---
+++ tests/expenseControllerSpec.js
@@ -0,0 +1,9 @@
+var should = require('should');
+var sinon = require('sinon');
+
+describe('Expense Controller Tests:', function () {
+
+    describe('Post', function () {
+        it('should not allow empty title on post')
+    });
+});
